7242685a7dd646e947233b00f7a8bf49c3be56e3
Maxim <bevzmaxim@ukr.net>
2021-05-01 14:08:34 +0300

    Create post-show functional

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,51 @@
+import {useState, useEffect} from 'react';
+import './Posts.scss';
+import Post from '../Post/Post';
+import PostInfo from "../PostInfo/PosntInfo";
+
+export default function Posts() {
+
+    let [posts, setPosts] = useState([]);
+    let [post, setPost] = useState( null);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(posts => {
+                setPosts([...posts]);
+            });
+    }, []);
+
+    const showInfo = (id) => {
+        const onePost = posts.find(el => el.id === id);
+
+        setPost(onePost.body);
+    }
+
+    const clearInfo = () => {
+        if(post !== null) {
+            setPost(post = null);
+        }
+        return
+    }
+
+    return (
+        <div className='posts'>
+            <ul className='post-list'>
+                {
+                    posts.map(item =>
+                        <Post
+                            key={item.id}
+                            postTitle={item}
+                            showInfo={showInfo}/>)
+                }
+            </ul>
+            <div className='post-info'>
+                {
+                    post ? <PostInfo post={post} clearInfo={clearInfo}/> : <PostInfo clearInfo={clearInfo}/>
+                }
+            </div>
+        </div>
+
+    );
+}
